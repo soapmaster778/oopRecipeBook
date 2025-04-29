16a19e715774c39af8c9f8d9aed6f0970592cfb2
soapmaster778 <evgen1730v@gmail.com>
2025-04-30 00:40:05 +0300

    Transformed to interface, added virtual destructor, pure virtual function print, GetType

--- CookbookItem.cpp
+++ CookbookItem.cpp
@@ -1,31 +1,5 @@
 #include "CookbookItem.h"
-#include <iostream>
 
-CookbookItem::CookbookItem(const std::string& name, const std::string& description)
-    : name(name), description(description) {
-    std::cout << "Constructor CookbookItem: " << name << std::endl;
-}
-
-CookbookItem::~CookbookItem() {
-    std::cout << "Destructor CookbookItem: " << name << std::endl;
-}
-
-void CookbookItem::setName(const std::string& newName) {
-    name = newName;
-}
-
-void CookbookItem::setDescription(const std::string& newDesc) {
-    description = newDesc;
-}
-
-std::string CookbookItem::getName() const {
-    return name;
-}
-
-std::string CookbookItem::getDescription() const {
-    return description;
-}
-
-void CookbookItem::display() const {
-    std::cout << "Item: " << name << "\n Description: " << description << std::endl;
+std::string CookbookItem::getType() const {
+    return "Generic Item";
 }
--- CookbookItem.h
+++ CookbookItem.h
@@ -1,23 +1,13 @@
 #ifndef COOKBOOKITEM_H
 #define COOKBOOKITEM_H
+
 #include <string>
 
 class CookbookItem {
-protected:
-    std::string name;
-    std::string description;
-
 public:
-    CookbookItem(const std::string& name = "None",
-                const std::string& description = "No description");
-    ~CookbookItem();
-
-    void setName(const std::string& newName);
-    void setDescription(const std::string& newDesc);
-    std::string getName() const;
-    std::string getDescription() const;
-
-    void display() const;
+    virtual void print() const = 0;
+    virtual std::string getType() const = 0;
+    virtual ~CookbookItem() = default;
 };
 
-#endif //COOKBOOKITEM_H
+#endif
